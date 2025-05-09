ac85151e622a993822fae62f5ed92cbe74ebae96
ramos-emily <ramos.emilyvi@gmail.com>
2025-05-09 16:20:46 -0300

    Hello world

--- first-spring-app/src/main/java/com/primeiroprojeto/first_spring_app/FirstSpringAppApplication.java
+++ first-spring-app/src/main/java/com/primeiroprojeto/first_spring_app/FirstSpringAppApplication.java
@@ -1,15 +1,16 @@
 package com.primeiroprojeto.first_spring_app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+// @Configuration //define que a classe pode ter metodos que define bens
+// @EnableAutoConfiguration //ativa autoconfig do spring
+// @ComponentScan //permite que o spring escaneie o pacte em busca das coisas necessarias e fazer o gerenciamento dessas dependencias
 
 @SpringBootApplication
-@Configuration //define que a classe pode ter metodos que define bens
-@EnableAutoConfiguration //ativa autoconfig do spring
-@ComponentScan //permite que o spring escaneie o pacte em busca das coisas necessarias e fazer o gerenciamento dessas dependencias
 public class FirstSpringAppApplication {
 
 	public static void main(String[] args) {
--- first-spring-app/src/main/java/com/primeiroprojeto/first_spring_app/controller/HelloWorldController.java
+++ first-spring-app/src/main/java/com/primeiroprojeto/first_spring_app/controller/HelloWorldController.java
@@ -1,13 +1,19 @@
 package com.primeiroprojeto.first_spring_app.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.ResponseBody;
 
-
-@Controller
+// @Controller
+// @ResponseBody
 @RestController //indica que a class é um controller rest, combinando o controller com rest
-@ResponseBody
+@RequestMapping("/hello-word")
 public class HelloWorldController {
-
+    //get, post, delete, put. patch, options, head
+    // GET /hello-world/get
+    @GetMapping
+    public String helloWorld(){
+        return "funcionaporfavor";
+    }
 }
